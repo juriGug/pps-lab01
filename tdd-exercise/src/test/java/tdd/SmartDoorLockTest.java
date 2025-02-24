94ce86e55f045a5ab6ef77087baf1bf8e81cfa56
Juri Guglielmi - juri.guglielmi@studio.unibo.it <juri.guglielmi@studio.unibo.it>
2025-02-24 10:38:25 +0100

    add implementation of interface and create the fisrt test

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,21 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    SmartDoorLockImpl smartPort;
+
+    @BeforeEach
+    public void createPort() {
+        smartPort = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testIsPortLock() {
+        assertFalse(smartPort.isLocked());
     }
 }
